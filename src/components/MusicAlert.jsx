e2f6fbb4be5c4bafd2906b8a469f383c7bf659b0
Bia Oliveira <pellegrini.beatriz@gmail.com>
2023-06-09 11:44:04 -0700

    small fixes to musicalert

--- src/components/MusicAlert.jsx
+++ src/components/MusicAlert.jsx
@@ -15,20 +15,22 @@ export default function MusicAlert({ track_name, artists }) {
 	return (
 		<div
 			className={`fixed bottom-16 right-0 hidden h-20 rounded-full bg-pink-200 transition-all duration-300 md:flex ${
-				expanded ? 'w-1/4' : 'w-1/12'
+				expanded ? 'md:w-1/3 lg:w-1/4' : 'md:w-1/6 lg:w-1/12'
 			}`}
 			onMouseEnter={handleHover}
 			onMouseLeave={handleMouseLeave}
 		>
 			<div className="flex items-center overflow-hidden">
 				<a href="/#music">
-					<FaAsterisk className="mx-6 h-10 w-10 text-orange-400 hover:animate-spin" />
+					<FaAsterisk className="mx-5 h-10 w-10 text-orange-400 hover:animate-spin lg:h-8 lg:w-8" />
 				</a>
 				{expanded && (
-					<p className="mt-9 h-20 w-52 text-sm">
-						Currently listening to{' '}
-						<span className="font-bold">{track_name}</span> by {artists}
-					</p>
+					<div className="track-info">
+						<p className="mx-auto text-xs md:w-36 lg:w-48 lg:text-sm">
+							Currently listening to{' '}
+							<span className="font-bold">{track_name}</span> by {artists}
+						</p>
+					</div>
 				)}
 			</div>
 		</div>
