d5ed688b19bb52db437b6581d2aaf659bb44d5e8
Bia Oliveira <pellegrini.beatriz@gmail.com>
2023-06-10 18:14:56 -0700

    recentered

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,7 +44,7 @@ export default function Home() {
 
 		return (
 			<>
-				<main className="flex flex-col overflow-x-hidden">
+				<main className="flex flex-col items-center overflow-x-hidden">
 					<Reveal>
 						<section className="intro-section z-30 flex h-72 flex-col items-center justify-center sm:h-96 md:h-screen">
 							<h1 className="fade-in z-30 font-eight text-4xl lowercase sm:text-6xl md:text-7xl">
