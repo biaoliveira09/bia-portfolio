5ae45bd3d3826212f85732a3f653e91c6b4e1c59
Bia Oliveira <pellegrini.beatriz@gmail.com>
2023-08-06 18:55:06 -0700

    Add all projects page back in

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,8 @@ import { useQuery } from '@tanstack/react-query';
 import Project from './Project';
 import { getProjects } from '../utilities/api';
 import Reveal from '../utilities/Reveal';
+import { BsArrowRightCircle } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 export const useProjects = () => {
 	const query = useQuery(['projects'], getProjects, {
@@ -24,6 +26,7 @@ export default function Projects() {
 	}
 
 	if (isSuccess && projects.length > 0) {
+		const firstThreeItems = projects.slice(0, 3);
 		return (
 			<>
 				<section
@@ -32,11 +35,11 @@ export default function Projects() {
 				>
 					<Reveal>
 						<h2 className="z-30 mb-3 text-3xl font-bold sm:mb-6 md:mb-10">
-							Projects
+							Featured Projects
 						</h2>
 					</Reveal>
 					<div className="projects-wrapper flex flex-col flex-wrap items-center justify-center gap-6 sm:mx-10 sm:gap-4 md:flex-row">
-						{projects.map(project => {
+						{firstThreeItems.map(project => {
 							const {
 								project_name,
 								project_overview,
@@ -61,6 +64,12 @@ export default function Projects() {
 							);
 						})}
 					</div>
+					<Link
+						to={`/projects`}
+						className="mt-10 flex items-center gap-2 text-lg  hover:-translate-y-0.5"
+					>
+						See All Projects <BsArrowRightCircle />
+					</Link>
 				</section>
 			</>
 		);
